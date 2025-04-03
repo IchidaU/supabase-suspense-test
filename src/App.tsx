5967858cc5236eb48d5234991299463331c7416b
IchidaU <koo.be.koo@gmail.com>
2025-04-03 23:06:14 +0900

    useを使用するパターン

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { lazy, Suspense } from "react";
-// import { RecordList } from "./components/RecordList";
+import { Suspense } from "react";
+import { RecordList } from "./components/RecordList";
 
 function App() {
-  const RecordList = lazy(() =>
-    import("./components/RecordList").then((module) => ({
-      default: module.RecordList,
-    }))
-  );
-
   return (
     <>
       <Suspense fallback={<div>読み込み中...</div>}>
--- src/components/RecordList.tsx
+++ src/components/RecordList.tsx
@@ -1,19 +1,8 @@
-import { useFetchData } from "../hooks/useFetchData";
+import { use } from "react";
+import { GetRecords } from "../lib/record";
 
 export const RecordList = () => {
-  const { records } = useFetchData();
-
-  // if (loading) {
-  //   return <div>読み込み中...</div>;
-  // }
-
-  // if (error) {
-  //   return <div>エラーが発生しました: {error.message}</div>;
-  // }
-
-  // if (!records || records.length === 0) {
-  //   return <div>データがありません</div>;
-  // }
+  const records = use(GetRecords());
 
   return (
     <div>
