969766460117ae9c954ee5d885e5d818339251b2
IchidaU <koo.be.koo@gmail.com>
2025-04-03 23:37:47 +0900

    カスタムフック導入

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,38 +1,8 @@
-import { useEffect, useState } from "react";
+import { use, useMemo } from "react";
 import { GetRecords } from "../lib/record";
-import { Record } from "../domain/record";
 
 export const useFetchData = () => {
-  const [records, setRecords] = useState<Record[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
+  const recordsPromise = useMemo(() => GetRecords(), []);
 
-  useEffect(() => {
-    console.log("useEffect in useFetchData is running");
-    const getAllRecords = async () => {
-      try {
-        console.log("fetching data");
-        const recordsData = await GetRecords();
-        console.log("Records fetched", recordsData);
-        setRecords(recordsData);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching records:", error);
-        setError(error instanceof Error ? error : new Error(String(error)));
-      }
-      setLoading(false);
-    };
-
-    getAllRecords();
-  }, []);
-
-  if (error) {
-    throw error;
-  }
-
-  //   if (loading) {
-  //     throw new Promise((resolve) => setTimeout(resolve, 1000));
-  //   }
-
-  return { records, loading, error };
+  return use(recordsPromise);
 };
